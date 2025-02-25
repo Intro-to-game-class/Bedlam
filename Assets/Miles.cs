f2152c52e939a5293a837b67bfc525630688baa0
JulesCastel <122455844+JulesCastel@users.noreply.github.com>
2025-02-25 14:05:41 -0600

    fixed Miles.cs
    had a misplaced curly bracket } causing a compilation error, so i fixed it.
    also edited style to match Unity C# standards

--- Assets/Miles.cs
+++ Assets/Miles.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
-public class Miles : MonoBehaviour{
+public class Miles : MonoBehaviour
+{
     private LMNTSpeech speech;
-}
-void Start() {
-    Speech = GetComponent<LMNTSpeech>();
-}
-void Update(){
 
-    Speech.dialogue = "Hi I'm Miles";
-    StartCoroutine(Speech.Talk());
-}
+    void Start()
+    {
+        speech = GetComponent<LMNTSpeech>();
+    }
+
+    void Update()
+    {
+        speech.dialogue = "Hi I'm Miles";
+        StartCoroutine(speech.Talk());
+    }
 }
